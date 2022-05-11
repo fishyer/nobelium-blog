8b7a08973b36c87a93a3668d3cc91fb592fe6eef
fishyer <630709658@qq.com>
2022-05-11 22:40:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+Blog
+Fork from nobelium.
---
+++ blog.config.js
@@ -0,0 +1,61 @@
+const BLOG = {
+  title: 'Fishyer-Blog',
+  author: 'fishyer',
+  email: 'yutianran666@gmail.com',
+  link: 'https://blog.fishyer.com',
+  description: '我有一个交流高效信息管理的微信群，如果感兴趣的话，欢迎加我微信拉你进群：fishyer2850，备注：入群',
+  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  appearance: 'auto', // ['light', 'dark', 'auto'],
+  font: 'serif', // ['sans-serif', 'serif']
+  lightBackground: '#FDFDF3', // use hex value, don't forget '#' e.g #fffefc
+  darkBackground: '#120E03', // use hex value, don't forget '#'
+  path: '', // leave this empty unless you want to deploy Nobelium in a folder
+  since: 2022, // If leave this empty, current year will be used.
+  postsPerPage: 10,
+  sortByDate: true,
+  showAbout: true,
+  showArchive: true,
+  autoCollapsedNavBar: false, // The automatically collapsed navigation bar
+  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
+  socialLink: 'https://logseq.fishyer.com',
+  seo: {
+    keywords: ['Blog', 'Website', 'Notion','Android','GTD','RSS','PKM','OKR','Logseq','Obsidian'],
+    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
+  },
+  notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
+  notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
+  analytics: {
+    provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    ackeeConfig: {
+      tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
+      dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
+      domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+    },
+    gaConfig: {
+      measurementId: 'G-9Z9QVXELHX' // e.g: G-XXXXXXXXXX
+    }
+  },
+  comment: {
+    // support provider: gitalk, utterances, cusdis
+    provider: 'cusdis', // leave it empty if you don't need any comment plugin
+    gitalkConfig: {
+      repo: '', // The repository of store comments
+      owner: '',
+      admin: [],
+      clientID: '',
+      clientSecret: '',
+      distractionFreeMode: false
+    },
+    utterancesConfig: {
+      repo: ''
+    },
+    cusdisConfig: {
+      appId: '7b3199af-548e-4019-a826-4ff139450e7f', // data-app-id
+      host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
+      scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
+    }
+  },
+  isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+}
+// export default BLOG
+module.exports = BLOG
