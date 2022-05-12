33c65778a092b02e55b953a3f3f5598a056c54ed
fishyer <630709658@qq.com>
2022-05-12 14:31:23 +0800

    fix-friends bug

--- components/Header.js
+++ components/Header.js
@@ -10,9 +10,10 @@ const NavBar = () => {
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
     { id: 2, name: locale.NAV.RSS, to: '/subscribe', show: true },
     { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true },
-    { id: 4, name: locale.NAV.SEARCH, to: '/friends', show: true },
+    { id: 4, name: locale.NAV.FRIENDS, to: '/friends', show: true },
     { id: 5, name: locale.NAV.LOGSEQ, to: 'https://logseq.fishyer.com', show: true },
-    { id: 6, name: locale.NAV.NOTION, to: 'https://notion.fishyer.com', show: true }
+    { id: 6, name: locale.NAV.NOTION, to: 'https://notion.fishyer.com', show: true },
+    { id: 7, name: locale.NAV.GITHUB, to: 'https://github.com/fishyer', show: true }
   ]
   return (
     <div className="flex-shrink-0">
